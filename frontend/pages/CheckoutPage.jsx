b6bba1a26da1bcade30f24f140b20de67a9d8070
Rahul Parihar <rahul72parihar@gmail.com>
2026-04-27 17:24:43 +0530

    cart page font changed

--- frontend/pages/CheckoutPage.jsx
+++ frontend/pages/CheckoutPage.jsx
@@ -2,13 +2,9 @@ import { useState, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { apiService } from '../services/api.js';
-import '../styles/CheckoutPage.css';
-      <h1>Checkout</h1>
-      <p>Checkout functionality coming soon!</p>
-      <div style={{marginTop: '2rem', fontSize: '1.2rem', color: '#666'}}>
-        Your cart will be processed here. For now, return to <a href="/cartpage" style={{color: '#667eea'}}>Cart</a>.
-      </div>
-    </div>
-  );
-}
+export default function CheckoutPage(){
+  return <h1>
+    In Development
+  </h1>
 
+}
